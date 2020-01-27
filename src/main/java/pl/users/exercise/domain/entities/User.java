59fcb1cad1190591f9551ad28b63a8e87a63b7f4
Maciej Dluzen <maciej.dluzen@yahoo.com>
2020-01-27 18:53:02 +0000

    User entity

---
+++ src/main/java/pl/users/exercise/domain/entities/User.java
@@ -0,0 +1,28 @@
+package pl.users.exercise.domain.entities;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter @Setter
+@EqualsAndHashCode
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String firstname;
+    @Column(nullable = false)
+    private String surname;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private Integer age;
+
+}
